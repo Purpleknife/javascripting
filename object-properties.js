cf549d8b687b74d1ca4b599cb236910df82d1bb0
Hind <hind.nayhi@gmail.com>
2022-07-04 20:21:13 -0600

    Working on Objects and Functions in NodeSchool's javascripting

---
+++ object-properties.js
@@ -0,0 +1,7 @@
+
+const food = {
+  types: 'only pizza'
+}
+
+//Use console.log() to print the types property of the food object to the terminal.
+console.log(food.types);
---
+++ objects.js
@@ -0,0 +1,9 @@
+
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni'],
+  crust: 'deep dish',
+  serves: 2
+}
+
+//Use console.log() to print the object pizza to the terminal.
+console.log(pizza);
